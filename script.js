579b3d21b3e1b87e20cdb3d615e35a283a283336
Vladyslav Kostenko <vlad.k@integrio.net>
2021-11-26 13:00:39 +0200

    Вывод различной информации в консоль

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 'use strict';
 
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = '',
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 120,
+    rollback = 50,
+    fullPrice = 1000,
+    adaptive = true;
 
-alert("Hello world!");
-console.log("Hello console!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани \n Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
+console.log(screens.toLowerCase().split(","));
+console.log("Процент отката " + (fullPrice*(rollback/100)));
