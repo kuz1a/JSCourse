6612139228012a7ceda8eba7d81041bae4dc7310
Vladyslav Kostenko <vlad.k@integrio.net>
2021-11-26 15:51:52 +0200

    Сделано усложненное задание

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ let title = '',
     fullPrice = 1000,
     adaptive = true;
 
+
+let num = 266219;
+let sum = 1;
+let multThree = 0;
+let arrayNum = num.toString().split('');
+console.log(arrayNum);
+for (let i = 0; i < arrayNum.length; i++) {
+  sum *= arrayNum[i];
+
+}
+multThree = sum ** 3;
+
 console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
@@ -14,3 +26,7 @@ console.log(screens.length);
 console.log("Стоимость верстки экранов " + screenPrice + " рублей/ долларов/гривен/юани \n Стоимость разработки сайта " + fullPrice + " рублей/ долларов/гривен/юани");
 console.log(screens.toLowerCase().split(","));
 console.log("Процент отката " + (fullPrice*(rollback/100)));
+
+console.log("Перемножение " + sum);
+console.log("Степень " + multThree);
+console.log("Первые две цифры числа это " + multThree.toString().slice(0,2) );
