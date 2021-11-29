9d4325b2ae10af1a4345d2b70bfa409fc7abb01a
Vladyslav Kostenko <vlad.k@integrio.net>
2021-11-29 12:43:04 +0200

    deleted hard lesson02 homework

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 'use strict';
-let num = 266219;
-let sum = 1;
-let multThree = 0;
-let arrayNum = num.toString().split('');
-
-for (let i = 0; i < arrayNum.length; i++) {
-  sum *= arrayNum[i];
-
-}
-
-// multThree = sum ** 3;
-// console.log("Перемножение " + sum);
-// console.log("Степень " + multThree);
-// console.log("Первые две цифры числа это " + multThree.toString().slice(0,2) );
-
 
 let title = prompt('Как называется ваш проект?');
 let screens = prompt('Какие типы экранов нужно разработать?', "Простые, Сложеные, Интерактивные");
