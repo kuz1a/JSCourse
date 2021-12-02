1d094fc9c9e9a8704bef032dae5128e97fe6eca0
Vladyslav Kostenko <vlad.k@integrio.net>
2021-12-02 11:41:44 +0200

    lesson06-hard

--- script.js
+++ script.js
@@ -13,32 +13,47 @@ const isNumber = (num) => {
 const start = () => {
 
   let numberAsk = getNumber(100);
-  
+  let k = 10;
 
   const game = () => {
     
     let number = prompt("Угадай число от 1 до 100");
     console.log( typeof number);
     if (number === null ) {
-      alert("game over сыграем")
+      alert("game over ")
+      return
+    }
+    if (k === 0) {
+      if(confirm('Попытка кончились не угадал еще?')){
+        start()
+      } else {
+        alert('Пока')
+        return;
+      }
+      
     }
 
     if (isNumber(number)) {
       let userNumber = +number;
       if (userNumber > numberAsk) {
         alert('Загаданное число меньше')
-        
+        k--;
         game();
       } else if (userNumber < numberAsk) {
         alert('Загаданное число больше');
+        k--;
         game();
-      } else if (userNumber === numberAsk) {
-        alert('Вы угадали число ' + numberAsk);
-      
+      } else {
+        k--;
+        if (confirm('Вы угадали число ' + numberAsk + ' и осталось еще ' + k +' попыток. Еще?')) {
+          start();
+        } else {
+            alert('Пока')
+            return
+        }
       }
     } else {
-      alert('Введите число')
-      game();
+      start();
     }
 
   }
