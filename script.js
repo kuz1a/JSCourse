b53458cd29288aa0679eaf3ee382ad6a940344b1
Vladyslav Kostenko <vlad.k@integrio.net>
2021-12-02 11:32:49 +0200

    home work done

--- script.js
+++ script.js
@@ -11,30 +11,39 @@ const isNumber = (num) => {
 
 
 const start = () => {
-
+  
   let numberAsk = getNumber(100);
   
+
 
   const game = () => {
     
     let number = prompt("Угадай число от 1 до 100");
-    console.log( typeof number);
     if (number === null ) {
-      alert("game over сыграем")
+      alert("Пока")
+      return
     }
+    
 
     if (isNumber(number)) {
       let userNumber = +number;
       if (userNumber > numberAsk) {
+        
         alert('Загаданное число меньше')
         
         game();
       } else if (userNumber < numberAsk) {
+       
         alert('Загаданное число больше');
+        
         game();
-      } else if (userNumber === numberAsk) {
-        alert('Вы угадали число ' + numberAsk);
-      
+      } else {
+        if (confirm('Вы угадали число ' + numberAsk + ' Еще? ')) {
+          start();
+        } else {
+          alert('Пока')
+          return
+        }
       }
     } else {
       alert('Введите число')
